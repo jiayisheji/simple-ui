ee5b6a1566b72e99357b17b95edf5373d4e85cc7
jiayi <690405541@qq.com>
2020-10-20 18:36:31 +0800

    feat(core): 添加fromJson方法安全处理JSON.stringify

--- libs/core/coercion/json.spec.ts
+++ libs/core/coercion/json.spec.ts
@@ -1,7 +1,59 @@
 import { primitives } from '@ngx-simple/core/testing';
 import { toJson } from './json';
 
-describe('toObservable', () => {
+describe('toJson', () => {
+  it('should coerce an any not string into an any', () => {
+    const expected = primitives.map((value, i) => i);
+    const actual = primitives.map(toJson);
+
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('should coerce an string array into an array', () => {
+    const expected = '[1,2,3]';
+
+    expect(toJson(expected)).toStrictEqual(expected);
+  });
+
+  it('should coerce an string object into an object', () => {
+    const expected = '{"a":1}';
+
+    expect(toJson(expected)).toStrictEqual(expected);
+  });
+
+  it('should coerce an object into an object', () => {
+    const values: [any, any][] = [
+      [null, null],
+      [{}, {}],
+      [[], []],
+      [new Error(), new Error()],
+      [1, null],
+      [undefined, null],
+      [true, null],
+      [NaN, null]
+    ];
+    const expected = values.map(value => value[1]);
+    const actual = values.map(value => toJson(value[0]));
+
+    expect(actual).toStrictEqual(expected);
+  });
+
+  it('should coerce an Unexpected end of JSON input into an fallbackValue', () => {
+    const values: [string, any][] = [
+      ['', null],
+      ['1', 1],
+      ['-1', -1]
+    ];
+    const expected = values.map(value => value[1]);
+    const actual = values.map(value => toJson(value[0], value[1]));
+
+    expect(actual).toStrictEqual(expected);
+
+    expect(toJson('')).toBeNull();
+  });
+});
+
+describe('fromJson', () => {
   it('should coerce an any not string into an any', () => {
     const expected = primitives.map((value, i) => i);
     const actual = primitives.map(toJson);
--- libs/core/coercion/json.ts
+++ libs/core/coercion/json.ts
@@ -3,23 +3,50 @@
  * js是动态语言，只保证使用`JSON.parse`处理字符串，其他值如果是对象直接返回，如果是其他值返回回退值
  *
  * @param value 任意值 期望是字符串
+ * @param fallbackValue 回退值
  * @returns 返回`JSON.parse`处理值
  */
 export function toJson<D>(value: string): D;
 export function toJson<D, T>(value: string, fallbackValue: D): T | D;
 export function toJson(value: string, fallbackValue = null) {
-  const type = typeof value;
   if (value == null) {
     return fallbackValue;
   }
-  if (type === 'object') {
+  if (typeof value !== 'string') {
     return value;
-  }
-  if (type !== 'string') {
-    return fallbackValue;
   }
   try {
     return JSON.parse(value);
+  } catch (_ignoreError) {
+    return fallbackValue;
+  }
+}
+
+/**
+ * 将数据绑定值(通常是`{}|[]`)强制转换为字符串
+ * js是动态语言，只保证使用`JSON.stringify`处理`{}|[]`，其他值如果处理失败直接返回回退值。
+ *
+ * @param value 任意值 期望是{}|[]
+ * @param fallbackValue 回退值
+ * @param config JSON.stringify 剩余参数 replacer 和 space
+ * @returns 返回`JSON.stringify`处理值
+ */
+export function fromJson(value: null | undefined): null;
+export function fromJson<D>(value: D): string;
+export function fromJson<D>(
+  value: D | null | undefined,
+  fallbackValue = null,
+  config?: {
+    replacer?: (this: any, key: string, value: any) => any;
+    space?: string | number;
+  }
+): string | null {
+  if (value == null) {
+    return fallbackValue;
+  }
+  try {
+    const { replacer, space } = config || {};
+    return JSON.stringify(value, replacer, space);
   } catch (_ignoreError) {
     return fallbackValue;
   }
