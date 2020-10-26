373667505f0f9745bcfb2726127726f2608d8cab
jiayi <690405541@qq.com>
2020-10-26 15:43:24 +0800

    feat(simple-ui-doc): 完善分割线文档

--- apps/simple-ui-doc/src/app/examples/divider-example/divider-example.module.ts
+++ apps/simple-ui-doc/src/app/examples/divider-example/divider-example.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { SimDividerModule } from '@ngx-simple/simple-ui/divider';
+import { DividerOverviewExample } from './divider-overview/divider-overview-example';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule]
+  declarations: [DividerOverviewExample],
+  imports: [CommonModule, SimDividerModule],
+  exports: [DividerOverviewExample]
 })
 export class DividerExampleModule {}
---
+++ apps/simple-ui-doc/src/app/examples/divider-example/divider-overview/divider-overview-example.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+/**
+ * @title Basic dividers
+ */
+@Component({
+  selector: 'doc-divider-overview-example',
+  templateUrl: 'divider-overview-example.html',
+  styleUrls: ['divider-overview-example.css']
+})
+export class DividerOverviewExample {}
